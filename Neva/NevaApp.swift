2b6efdb18411b0bcb4dbbb65ade2660180685ecb
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-10-28 10:13:06 +0300

    Initial Commit

---
+++ Neva/NevaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NevaApp.swift
+//  Neva
+//
+//  Created by Abdelrahman Mohamed on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct NevaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NevaTests/NevaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NevaTests.swift
+//  NevaTests
+//
+//  Created by Abdelrahman Mohamed on 28.10.2025.
+//
+
+import Testing
+@testable import Neva
+
+struct NevaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
