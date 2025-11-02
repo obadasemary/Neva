06c897fed86f306b7ad8de8d271eea7ca48e0d9f
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-11-02 21:08:16 +0300

    feat: implement `RemoteImage`

---
+++ Neva/RemoteImage.swift
@@ -0,0 +1,55 @@
+//
+//  RemoteImage.swift
+//  Neva
+//
+//  Created by Abdelrahman Mohamed on 02.11.2025.
+//
+
+import SwiftUI
+
+struct RemoteImage: View {
+    
+    let url: URL?
+    let imageSize: CGFloat
+    
+    init(url: URL?, imageSize: CGFloat = 100) {
+        self.url = url
+        self.imageSize = imageSize
+    }
+    
+    var body: some View {
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.quinary)
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            case .failure:
+                Image(systemName: "exclamationmark.triangle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+                    .foregroundStyle(.secondary)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.quinary)
+            @unknown default:
+                EmptyView()
+            }
+        }
+
+    }
+}
+
+#Preview {
+    RemoteImage(
+        url: URL(
+            string: "https://picsum.photos/600/600"
+        )
+    )
+}
