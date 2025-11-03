31a978adf5288ca5b7d47a14358f6e5b84390dc9
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-11-03 08:10:43 +0300

    feat: add app-wide constants for URLs and UI dimensions

---
+++ Neva/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Neva
+//
+//  Created by Abdelrahman Mohamed on 28.10.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let url = URL(string: "https://rickandmortyapi.com/api/character")
+    static let randomImage = "https://picsum.photos/600/600"
+    static let imageDimanothions: CGFloat = 100
+    static let cornerRadius: CGFloat = 4
+}
