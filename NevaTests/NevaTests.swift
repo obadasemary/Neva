4e7cab469e3e54a4ce2c8e594fd047d19b71383d
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-11-03 08:10:51 +0300

    test: add unit tests for FeedViewModel with success and failure cases

--- NevaTests/NevaTests.swift
+++ NevaTests/NevaTests.swift
@@ -6,12 +6,84 @@
 //
 
 import Testing
+import Foundation
 @testable import Neva
 
 struct NevaTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @MainActor
+    @Test("Test Fetch Feed with Success")
+    func fetchCharacters_onSuccess() async throws {
+        let response = [
+            CharacterResponse(
+                id: 1,
+                name: "Obada",
+                species: "Human",
+                image: nil
+            ),
+            CharacterResponse(
+                id: 1,
+                name: "Obada",
+                species: "Human",
+                image: nil
+            )
+        ]
+        
+        let makeSut = {
+            MockFeedUseCase(
+                result:
+                        .success(
+                            FeedEntity(
+                                info: ResultPageInfo(count: 1, pages: 1),
+                                results: response
+                            )
+                        )
+            )
+        }
+        
+        let viewModel = FeedViewModel(feedUseCase: makeSut())
+        
+        await viewModel.loadData()
+        
+        #expect(viewModel.characters == response)
     }
+    
+    @MainActor
+    @Test("Test Fetch Feed with Failure")
+    func fetchCharacters_onFailure() async throws {
+        let makeSut = {
+            MockFeedUseCase(result: .failure(MockError.stub))
+        }
+        
+        let viewModel = FeedViewModel(feedUseCase: makeSut())
+        
+        await viewModel.loadData()
+        
+        #expect(viewModel.characters.isEmpty)
+    }
+}
 
+private extension NevaTests {
+    
+    enum MockError: Error {
+        case stub
+    }
+    
+    final class MockFeedUseCase: FeedUseCaseProtocol {
+        
+        private let result: Result<FeedEntity, Error>
+        
+        init(result: Result<FeedEntity, Error>) {
+            self.result = result
+        }
+        
+        func fetchFeed(url: URL) async throws -> FeedEntity {
+            switch result {
+            case .success(let response):
+                response
+            case .failure(let error):
+                throw error
+            }
+        }
+    }
 }
