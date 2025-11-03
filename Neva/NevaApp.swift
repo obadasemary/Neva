513b2319e18b95d8543d80f50629d0b2fc45cb35
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-11-03 08:10:50 +0300

    refactor: update NevaApp to use FeedBuilder for view assembly

--- Neva/NevaApp.swift
+++ Neva/NevaApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct NevaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let feedBuilder = FeedBuilder()
+            feedBuilder.buildFeedView()
         }
     }
 }
